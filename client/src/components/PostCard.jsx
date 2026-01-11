f11da365e997b50a321ab2b0cde3dc11273cbf9d
Shahid Ameen <mern04@724.one>
2026-01-11 21:53:16 +0500

    messages screen done

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { BadgeCheck } from 'lucide-react'
+import React, { useState } from 'react'
+import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react'
 import moment from 'moment'
+import { dummyUserData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const PostCard = ({ post }) => {
   if (!post) return null
 
+  const [likes, setLikes] = useState(post.likes_count);
+  const currentuser = dummyUserData;
+
+  const navigate = useNavigate();
+  const handleLikes = () => {
+
+  }
   const postwithhashtags = post.content?.replace(/#(\w+)/g, '<span class="text-indigo-600 cursor-pointer">#$1</span>'); 
   return (
     <div className='rounded-xl bg-white shadow p-4 space-y-4 w-full max-w-2xl'>
       {/* user info */}
-      <div className='inline-flex items-center gap-3 cursor-pointer'>
+      <div onClick={() => navigate('/profile/' + post.user._id)} className='inline-flex items-center gap-3 cursor-pointer'>
         <img src={post.user?.profile_picture} alt="" className='w-10 h-10 shadow rounded-full'/>
         <div>
           <div className='items-center space-x-1 flex'>
@@ -26,9 +35,28 @@ const PostCard = ({ post }) => {
     </div>}
       {/* post image */}
       <div className='grid grid-cols-2 gap-2'>
-     {post.image_urls.map((img,index) =>(
-     <img src={img} key={index} alt='images'  className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`}/>
-     )) }
+         {post.image_urls?.map((img,index) =>(
+         <img src={img} key={index} alt='images'  className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 ? 'col-span-2 h-auto' : ''}`}/>
+         )) }
+      </div>
+
+      {/* actions */}
+      <div className='flex items-center gap-4 text-gray-600 text-sm pt-4 border-t border-gray-300'>
+       <div className='flex items-center gap-1'>
+        <Heart
+          className={`w-4 h-4 cursor-pointer ${likes.includes(currentuser._id) ? 'text-red-500 fill-red-500' : ''}`}
+          onClick={handleLikes}
+        />
+        <span>{likes.length}</span>
+       </div>
+        <div className='flex items-center gap-1'>
+          <MessageCircle className='w-4 h-4'/>
+        <span>{12}</span>
+       </div>
+        <div className='flex items-center gap-1'>
+        <Share2 className='w-4 h-4'/>
+        <span>{7}</span>
+       </div>
       </div>
     </div>
   )
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '../components/Loading';
-import { dummyPostsData } from '../assets/assets';
+import { assets, dummyPostsData } from '../assets/assets';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
+import RecentMessages from '../components/RecentMessages';
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -29,11 +30,15 @@ const Feed = () => {
       </div>
 
          {/* right sidebar */}
-         <div>
-         <div>
-          <h1>Sponsored</h1>
+         <div className='max-xl:hidden sticky top-0'>
+         <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} alt="sponsored image" className='w-75 h-50 rounded-md'/>
+          <p className='text-slate-600'>Email Marketing</p>
+          <p className='text-slate-400'>Reach your audience effectively with our targeted email campaigns.Super Charge your marketing with a powerful, easy-to-use platform built for results.
+          </p>
          </div>
-         <h1>Recent messages </h1>
+         <RecentMessages/>
          </div>
     </div>
   ) :  <Loading/>
